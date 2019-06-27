0806765d0ca7233b9c3361a34bbb0128bb1b149f
Wiltonadm <49535876+Wiltonadm@users.noreply.github.com>
2019-06-27 16:45:47 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,15 +1,16 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
-
-import java.sql.PreparedStatement;
-
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -87,6 +88,48 @@ public class SellerDaoJDBC implements SellerDao {
 	@Override
 	public List<Seller> findAll() {
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " 
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id " 
+					+ "WHERE DepartmentId = ? " 
+					+ "ORDER BY Name ");
+
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			
+			List <Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			
+			while (rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+				dep = instatiteDepartmente(rs);
+				map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instatiteSeller(rs, dep);
+				list.add(obj);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+
 	}
 
 }
