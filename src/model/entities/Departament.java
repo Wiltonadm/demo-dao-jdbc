b4a4430769ca5280003daa430f2ac430cdca1871
Wiltonadm <49535876+Wiltonadm@users.noreply.github.com>
2019-06-27 14:10:02 -0300

    Department entity class

---
+++ src/model/entities/Departament.java
@@ -0,0 +1,67 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Departament implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer Id;
+	private String name;
+	
+	public Departament () {
+		
+	}
+
+	public Departament(Integer id, String name) {
+		Id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departament other = (Departament) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Departament [Id=" + Id + ", name=" + name + "]";
+	}
+}
