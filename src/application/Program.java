fe34ce6f2906a0f618b95d586a1d17a4eff87aa0
Wiltonadm <49535876+Wiltonadm@users.noreply.github.com>
2019-06-27 14:59:06 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		Departament obj = new Departament(1, "Boks");
 		
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 		
 	}
 
